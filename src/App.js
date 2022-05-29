5d21a55f37222f642138dadc9279b9c0099bcd2e
ilqar <pvtnumber50@protonmail.com>
2022-05-29 18:26:44 +0430

    button functionality, only DEL remains

--- src/App.js
+++ src/App.js
@@ -1,36 +1,87 @@
+import { useState } from 'react';
+
 function App() {
   
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if (
+      ops.includes(value) && calc === '' || 
+      ops.includes(value) && ops.includes(calc.slice(-1))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  }
+
+
   const createDigits = () => {
   const digits = [];
   for (let i = 1; i < 10; i++) {
     digits.push(
-      <button key={i}>{i}</button>
+      <button onClick={() => updateCalc(i.toString())} key={i}>
+        {i}
+      </button>
     )
   }
   return digits;
 }
+
+  //evaluates the  string when pressing = button
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
   return (
     <div className="App">
        <div className="calculator">
-          <div className="display">
-             <span>
-                (0)
-             </span>
-                0
+          <div className="top">
+                <div className="display">
+                  { result ? <span>
+                        {result}
+                      </span> 
+                    :
+                    ''
+                    }
+                    
+                   <span>
+                      &nbsp;&nbsp;  {/*HTML WHITE SPACE*/}
+                   </span>
+                         {calc || 0}
+                </div>
              <div className="operators">
-                <button className="operator"> / </button> 
-                <button className="operator"> * </button> 
-                <button className="operator"> - </button> 
-                <button className="operator"> + </button>  
-                <button className="operator"> = </button>
+                <button className="operator" 
+                 onClick = {() => updateCalc('/')}>
+                 / </button> 
+                <button className="operator"
+                 onClick = {() => updateCalc('*')}
+                > * </button> 
+                <button className="operator"
+                 onClick = {() => updateCalc('-')}
+                > - </button> 
+                <button className="operator"
+                 onClick = {() => updateCalc('+')}
+                > + </button>  
+               
 
                 <button>DEL</button>
              </div>
              <div className="digits">
                 { createDigits() }
-                <button>0</button>
-                <button>.</button>
-                <button>=</button>
+                <button onClick={() => updateCalc('0')}>
+                  0
+                </button>
+                <button onClick={() => updateCalc('.')}>
+                .
+                </button>
+                <button onClick={calculate}>
+                =
+                </button>
              </div>
           </div>
         </div>
